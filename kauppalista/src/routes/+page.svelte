9340a065c6e6fa1312d63db10d65d30ad41c7e55
Tuomas Suutari <tuomas@nepnep.net>
2024-04-15 13:57:16 +0300

    peruskokeilua

--- kauppalista/src/routes/+page.svelte
+++ kauppalista/src/routes/+page.svelte
@@ -1,4 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+    let asiat = ['Leipä', 'Tomaatti', 'Kurkku'];
+
+    let uusiAsia = '';
+
+    function lisääAsia() {
+        asiat.push(uusiAsia);
+        asiat = asiat;
+        uusiAsia = '';
+    }
+</script>
+
+<div class="komponentti">
+    <h1>Kauppalista</h1>
+    <ul>
+        {#each asiat as asia}
+            <li>
+                <input type="checkbox" />
+                {asia}
+            </li>
+        {/each}
+    </ul>
+    <form class="uusi">
+        <label for="uusi-asia">Lisää uusi asia:</label>
+        <input id="uusi-asia" type="text" bind:value={uusiAsia} />
+        <button on:click|preventDefault={lisääAsia}>Lisää</button>
+    </form>
+</div>
+
+<style>
+    .komponentti {
+        color: hwb(247 12% 74%);
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 12px;
+    }
+    h1 {
+        font-size: 200%;
+    }
+    ul {
+        font-size: 150%;
+    }
+    li {
+        list-style-type: none;
+    }
+    .uusi {
+        font-size: 125%;
+    }
+    label {
+        display: block;
+    }
+</style>
