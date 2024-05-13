e1bce68b437e96ec9e86a26985ccd286b3b99e66
Tuomas Suutari <tuomas@nepnep.net>
2024-05-13 13:59:30 +0300

    Siirrä koodia Kauppalista-komponenttiin

--- kauppalista/src/lib/components/Kauppalista.svelte
+++ kauppalista/src/lib/components/Kauppalista.svelte
@@ -1,16 +1,57 @@
 <script>
+    import {createEventDispatcher} from 'svelte';
+
     import Asia from './Asia.svelte';
 
     export let asiat;
+
+    const dispatch = createEventDispatcher();
+    let uusiAsiaTeksti = '';
 </script>
 
-<ul>
-    {#each asiat as asia (asia.id)}
-        <Asia {asia} on:poista-asia on:asian-valmis-muuttui />
-    {/each}
-</ul>
+<div class="kauppalista">
+    <h1>Kauppalista</h1>
+    <ul>
+        {#each asiat as asia (asia.id)}
+            <Asia {asia} on:poista-asia on:asian-valmis-muuttui />
+        {/each}
+    </ul>
+    <form
+        class="uusi"
+        on:submit|preventDefault={() => {
+            dispatch('uusi-asia', {teksti: uusiAsiaTeksti.trim()});
+            uusiAsiaTeksti = '';
+        }}
+    >
+        <label for="uusi-asia">Lisää uusi asia:</label>
+        <!-- svelte-ignore a11y-autofocus -->
+        <input
+            id="uusi-asia"
+            name="asia"
+            type="text"
+            bind:value={uusiAsiaTeksti}
+            required
+            autofocus
+        />
+        <button>Lisää</button>
+    </form>
+</div>
 
 <style>
+    .kauppalista {
+        color: hwb(120 5% 70%);
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 12px;
+    }
+    h1 {
+        font-size: 200%;
+    }
+    .uusi {
+        font-size: 125%;
+    }
+    label {
+        display: block;
+    }
     ul {
         font-size: 150%;
     }
--- kauppalista/src/routes/+page.svelte
+++ kauppalista/src/routes/+page.svelte
@@ -3,13 +3,9 @@
     import Kauppalista from '$lib/components/Kauppalista.svelte';
     import {kauppalista} from '$lib/stores.js';
 
-    let uusiAsiaTeksti = '';
-    let uusiAsiaVirhe = null;
-
     function lisääAsia(e) {
-        const teksti = uusiAsiaTeksti.trim();
+        const {teksti} = e.detail;
         const asia = {id: String(Math.random()), teksti};
-        uusiAsiaTeksti = '';
         $kauppalista = [...$kauppalista, asia];
     }
 
@@ -25,44 +21,9 @@
 </script>
 
 <Ilmoitus />
-<div class="komponentti">
-    <h1>Kauppalista</h1>
-    <Kauppalista
-        asiat={$kauppalista}
-        on:poista-asia={poistaAsia}
-        on:asian-valmis-muuttui={käsitteleValmisMuutos}
-    />
-    {#if uusiAsiaVirhe}
-        <p class="error">{uusiAsiaVirhe}</p>
-    {/if}
-    <form class="uusi" on:submit={lisääAsia}>
-        <label for="uusi-asia">Lisää uusi asia:</label>
-        <!-- svelte-ignore a11y-autofocus -->
-        <input
-            id="uusi-asia"
-            name="asia"
-            type="text"
-            bind:value={uusiAsiaTeksti}
-            required
-            autofocus
-        />
-        <button>Lisää</button>
-    </form>
-</div>
-
-<style>
-    .komponentti {
-        color: hwb(120 5% 70%);
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        font-size: 12px;
-    }
-    h1 {
-        font-size: 200%;
-    }
-    .uusi {
-        font-size: 125%;
-    }
-    label {
-        display: block;
-    }
-</style>
+<Kauppalista
+    asiat={$kauppalista}
+    on:uusi-asia={lisääAsia}
+    on:poista-asia={poistaAsia}
+    on:asian-valmis-muuttui={käsitteleValmisMuutos}
+/>
